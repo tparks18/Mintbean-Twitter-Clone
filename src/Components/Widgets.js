b711a7bfe153e7509b2181ed61b4bfc3fead2e43
Taty <tatyanaparks@gmail.com>
2021-05-08 22:01:31 -0500

    wrap up widget section

--- src/Components/Widgets.js
+++ src/Components/Widgets.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import "./Widgets.css";
+import {
+    TwitterTimelineEmbed,
+    TwitterShareButton,
+    TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Widgets() {
     return (
-        <div className='widgets'>
-            <h2>Widgets</h2>
-            
+      <div className="widgets">
+        <div className="widgets__input">
+          <SearchIcon className="widgets__searchIcon" />
+          <input placeholder="Search Twitter" type="text" />
         </div>
-    )
+
+        <div className="widgets__widgetContainer">
+          <h2>What's happening</h2>
+          <TwitterTweetEmbed tweetId={"1365434560461340676"} />
+          <TwitterTimelineEmbed
+            sourceType="profile"
+            screenName="Mintbeanio"
+            options={{ height: 400 }}
+          />
+          <TwitterShareButton
+            url={"https://mintbean.io/"}
+            options={{ text: "#Mintbean is awesome!", via: "lxsmaresdelsur" }}
+          />
+        </div>
+      </div>
+    );
 }
 
 export default Widgets
